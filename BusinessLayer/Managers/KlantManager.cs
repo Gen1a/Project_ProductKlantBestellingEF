f94db738dbb96cc1a3d3882b638db858e9f84e66
Gen1a <frederikdebrouwer@gmail.com>
2021-01-04 21:47:47 +0100

    Added Managers

---
+++ BusinessLayer/Managers/KlantManager.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Exceptions;
+using BusinessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Managers
+{
+    public class KlantManager
+    {
+        private Dictionary<int, Klant> _klanten = new Dictionary<int, Klant>();
+        public IReadOnlyList<Klant> GeefKlanten()
+        {
+            return new List<Klant>(_klanten.Values).AsReadOnly();
+        }
+        public void VoegKlantToe(Klant klant)
+        {
+            if (_klanten.ContainsKey(klant.KlantId))
+            {
+                throw new KlantManagerException("Klant is reeds aanwezig");
+            }
+            else
+            {
+                _klanten.Add(klant.KlantId, klant);
+            }
+        }
+        public void VerwijderKlant(Klant klant)
+        {
+            if (!_klanten.ContainsKey(klant.KlantId))
+            {
+                throw new KlantManagerException("Klant is niet aanwezig");
+            }
+            else
+            {
+                _klanten.Remove(klant.KlantId);
+            }
+        }
+        public Klant GeefKlant(int klantId)
+        {
+            if (!_klanten.ContainsKey(klantId))
+            {
+                throw new KlantManagerException("Klant is niet aanwezig");
+            }
+            else
+            {
+                return _klanten[klantId];
+            }
+        }
+    }
+}
