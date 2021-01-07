aa17f06f0a8ee122e4577842e3cab9d81923d12e
Gen1a <frederikdebrouwer@gmail.com>
2021-01-08 00:12:36 +0100

    Added IDbManager interface

---
+++ BusinessLayer/Interfaces/IDbManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IDbManager<T>
+    {
+        IReadOnlyList<T> HaalOp();
+        IReadOnlyList<T> HaalOp(Func<T, bool> predicate);
+        void VoegToe(T item);
+        void Verwijder(T item);
+        T HaalOp(long id);
+    }
+}
