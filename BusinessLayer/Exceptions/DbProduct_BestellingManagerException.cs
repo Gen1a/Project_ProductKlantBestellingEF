77a02d806363cf39b468c2fa80a016d4bda08a27
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-01-11 20:49:35 +0100

    Added Product_BestellingExceptions

---
+++ BusinessLayer/Exceptions/DbProduct_BestellingManagerException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BusinessLayer.Exceptions
+{
+    public class DbProduct_BestellingManagerException : Exception
+    {
+        public DbProduct_BestellingManagerException()
+        {
+        }
+
+        public DbProduct_BestellingManagerException(string message) : base(message)
+        {
+        }
+
+        public DbProduct_BestellingManagerException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ BusinessLayer/Exceptions/Product_BestellingException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BusinessLayer.Exceptions
+{
+    public class Product_BestellingException : Exception
+    {
+        public Product_BestellingException()
+        {
+        }
+
+        public Product_BestellingException(string message) : base(message)
+        {
+        }
+
+        public Product_BestellingException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
+
