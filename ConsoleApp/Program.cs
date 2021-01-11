7e383fa9e609f5d00dbaceea47d28f9318cda2bb
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-01-11 20:52:03 +0100

    Added Configurationmanager (conn string)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Managers;
 using BusinessLayer.Interfaces;
 using BusinessLayer.Models;
+using System.Configuration;
 using BusinessLayer.Factories;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,31 @@ namespace ConsoleAppKlantBestellingen
         static void Main(string[] args)
         {
             Console.WriteLine("ProductKlantBestelling ConsoleApp:");
-            
-            string connectionString = "Data Source=HP-ENVYX360;Initial Catalog=Project_ProductKlantBestelling;Integrated Security=True";
-            IDbManager<Klant> kM = new DbKlantManager(connectionString);
-            IDbManager<Product> pM = new DbProductManager(connectionString);
-            IDbManager<Bestelling> bM = new DbBestellingManager(connectionString);
-            Klant k = kM.HaalOp(1);
-            kM.VoegToe(new Klant("Elena", "Geraardsbergsesteenweg"));
-            
+
+            // DATABASE VERSION
+            //string connectionString = ConfigurationManager.ConnectionStrings["HPEnvy"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["HPZBook"].ConnectionString;
+
+            DbKlantManager kM = new DbKlantManager(connectionString);
+            DbProductManager pM = new DbProductManager(connectionString);
+            DbBestellingManager bM = new DbBestellingManager(connectionString);
+
+            Klant k1 = kM.HaalOp(1);
+
+            //Product p1 = pM.MaakNieuwProduct("Chaudfontaine", 0.99m);
+            //Product p2 = pM.MaakNieuwProduct("Vittel", 2.3m);
+            //Product p3 = pM.MaakNieuwProduct("Contrex", 1.8m);
+
+            Dictionary<Product, int> producten1 = new Dictionary<Product, int>();
+            foreach(var product in pM.HaalOp(x => x.Prijs < 2.0m)){
+                producten1.Add(product, 3);
+            }
+
+            Bestelling b1 = bM.MaakNieuweBestelling(DateTime.Now, k1, producten1);
+
+            bM.VoegToe(b1);
+
+
 
             // MEMORY VERSION
             //KlantManager kM = new KlantManager();
