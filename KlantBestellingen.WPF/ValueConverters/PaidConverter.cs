aef0bccc0c801ec6ead1e2758aecfea563abe686
Frederik De Brouwer <frederik.debrouwer@cegeka.com>
2021-01-16 21:46:52 +0100

    Added boolean Paidconverter

---
+++ KlantBestellingen.WPF/ValueConverters/PaidConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using KlantBestellingen.WPF.Languages;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KlantBestellingen.WPF.ValueConverters
+{
+    public class PaidConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string output = Translations.No;
+            if ((bool)value)
+                output = Translations.Yes;
+
+            return output;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
